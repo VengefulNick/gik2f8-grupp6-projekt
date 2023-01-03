af23e1bf95f4bcd4acdf4c27b3e8fdbaec068cf4
VengefulNick <89831583+VengefulNick@users.noreply.github.com>
2023-01-03 14:39:26 +0100

    start on script

--- client/script.js
+++ client/script.js
@@ -1,5 +1,10 @@
 // EventListeners
 
+addUserForm.addEventListener("submit", onSumbit);
+
+const userContainerElement = document.getElementById('cardContainer');
+const api = new Api("http://localhost:5000/users");
+
 // InputValidation
 
 // SaveFunc
